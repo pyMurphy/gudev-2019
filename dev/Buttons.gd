7092dfba9217b20f25bf19f62399f87f17b3282e
pyMurphy <murphy@activist.com>
2019-09-28 20:53:02 +0100

    Hourly backup

--- dev/Buttons.gd
+++ dev/Buttons.gd
@@ -3,7 +3,6 @@ extends Control
 func _ready():
 	get_node('Exit').connect('pressed',self,'_on_Exit_pressed')
 	get_node('Start').connect('pressed',self,'_on_Start_pressed')
-	get_node('Controls').connect('pressed',self,'_on_Controls_pressed')
 
 func _on_Exit_pressed():
 	get_tree().quit()
@@ -11,11 +10,8 @@ func _on_Exit_pressed():
 func _on_Start_pressed():
 	get_tree().change_scene('res://Game.tscn')
 
-func _on_Controls_pressed():
-	get_tree().change_scene('res://Controls.tscn')
-
 func _on_Start_mouse_entered():
-	get_node("Start").rect_scale = Vector2(0.41,0.41)
+	get_node("Start").rect_scale += Vector2(0.01,0.01)
 
 func _on_Start_mouse_exited():
-	get_node("Start").rect_scale = Vector2(0.4,0.4)
+	get_node("Start").rect_scale -= Vector2(0.01,0.01)
